656f813799849e59eb8bf4ea50d0b790a29c1f2b
Greg Lauckhart <greg@lauckhart.com>
2024-09-02 23:39:00 -0700

    Initial decomposition pass on matter.js
    This is a partial pass deconstructing the matter.js package.  I tried to stick to decisions we'd previously agreed to.
    
    The new @project-chip/matter.js-general package contains those portions of matter.js that are not Matter specific.
    
    The new @project-chip/matter.js-model package contains the Matter object model.
    
    Since I was moving stuff around I got rid of the Uint8Array monkey patches we did in ByteArray.  For the most part this just consisted of replacing the "ByteArray" type with "Uint8Array".  The methods we injected I moved to a "Bytes" namespace containing utility functions.  This is a breaking change.
    
    Following new conventions, the new packages don't have "matter.js" in their names, so they're just in "packages/general" and "packages/model".  I also renamed packages/matter.js-tools to packages/tools.  I did not rename the other directories yet.
    
    I updated codegen and the intermediate model package dependencies, so codegen errors do not prevent codegen from running except for the generated model files in @project-chip/matter.js-model.
    
    The new packages are smaller so I moved to a flat namespace for exports.  So there are no subpaths exported from the "general" package, even though most of the exports were previously exported as subpaths of matter.js.  I think this improves readability and makes consumption simpler.
    
    "model" also has a flat namespace but this was true of the previous "@project-chip/matter.js/model" module so this isn't as big of a change.
    
    I did my best to add exports to matter.js to support backward compatibility.  However I used a little editorial control to select which exports to reproduce at the old location...  I made sure to cover all of those we used previously in the examples.  Many of the other exports were probably not ever used externally.  Shouldn't be a big deal if I got it wrong; we can add additional exports or just inform users of the new locations.
    
    Fixes #156

---
+++ src/util/node-shims.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright 2022-2024 Matter.js Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import "mocha";
+import { webcrypto } from "node:crypto";
+
+// Required for Node < 19
+if (globalThis.crypto === undefined) {
+    Object.assign(globalThis, { crypto: webcrypto });
+}
